b887d074b06b615728c3eefc4855ec47920e6966
Bernardo <tiezzibernardo96@gmail.com>
2025-01-28 17:22:36 +0100

    Refactored Hello World class another time
    https://github.com/Tiezzi96/java-examples/issues/1

--- java.helloworld/src/java/helloworld/HelloWorld.java
+++ java.helloworld/src/java/helloworld/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	private static HelloMessage helloMessage;
 
 	public static void main(String[] args) {
-		helloMessage = new HelloMessage("Hello World!");
+		helloMessage = new HelloMessage("Hello World from refactor!");
 		System.out.println(helloMessage.getMessage());
 	}
 }
