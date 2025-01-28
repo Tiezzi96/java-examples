fdbd60bd4cf15127da3cf1f495b2bf9a298dc291
Bernardo <tiezzibernardo96@gmail.com>
2025-01-28 02:24:01 +0100

    modified hello() from experiments

--- java.helloworld/src/java/helloworld/HelloWorld.java
+++ java.helloworld/src/java/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package java.helloworld;
 public class HelloWorld {
 
 	private static String hello() {
-		return "Hello Wolrd";
+		return "Hello Wolrd!";
 	}
 	public static void main(String[] args) {
 		System.out.println(hello());
