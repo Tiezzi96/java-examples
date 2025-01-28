bf9681781f0e9a7bcbd92ebaaa44e5896bceef4a
Bernardo <tiezzibernardo96@gmail.com>
2025-01-28 01:49:41 +0100

    Added "Hello" method to class "HelloWorld"

--- java.helloworld/src/java/helloworld/HelloWorld.java
+++ java.helloworld/src/java/helloworld/HelloWorld.java
@@ -2,6 +2,9 @@ package java.helloworld;
 
 public class HelloWorld {
 
+	public static String Hello() {
+		return "Hello Wolrd";
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello Wolrd");
 	}
