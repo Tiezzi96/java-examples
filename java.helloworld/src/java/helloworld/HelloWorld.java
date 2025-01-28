f224b2818df7d7a31e65a1144125f35f4e4c17e6
Bernardo <tiezzibernardo96@gmail.com>
2025-01-28 02:24:49 +0100

    modified helloworld from master second time

--- java.helloworld/src/java/helloworld/HelloWorld.java
+++ java.helloworld/src/java/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package java.helloworld;
 public class HelloWorld {	
 
 	private static String hello() {
-		return "Hello World";
+		return "Hello World!";
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello Wolrd from master, but changed");
