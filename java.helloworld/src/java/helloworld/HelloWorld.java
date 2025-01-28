c1b2c73960b9262c04140544e4281127a1455c05
Bernardo <tiezzibernardo96@gmail.com>
2025-01-28 01:53:15 +0100

    Modified "Hello" method and "main" method in HelloWorld class

--- java.helloworld/src/java/helloworld/HelloWorld.java
+++ java.helloworld/src/java/helloworld/HelloWorld.java
@@ -2,10 +2,10 @@ package java.helloworld;
 
 public class HelloWorld {
 
-	public static String Hello() {
+	private static String Hello() {
 		return "Hello Wolrd";
 	}
 	public static void main(String[] args) {
-		System.out.println("Hello Wolrd");
+		System.out.println(Hello());
 	}
 }
