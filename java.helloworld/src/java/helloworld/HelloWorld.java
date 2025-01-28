7a0b6fb17f6a182c07466f909d2b1d14c0207313
Bernardo <tiezzibernardo96@gmail.com>
2025-01-28 02:02:07 +0100

    Removed hello method

--- java.helloworld/src/java/helloworld/HelloWorld.java
+++ java.helloworld/src/java/helloworld/HelloWorld.java
@@ -2,10 +2,7 @@ package java.helloworld;
 
 public class HelloWorld {
 
-	private static String Hello2() {
-		return "Hello Wolrd";
-	}
 	public static void main(String[] args) {
-		System.out.println(Hello2());
+		System.out.println("Hello Wolrd");
 	}
 }
